ce365de7170f8c8af5b92224b82a54cc7992536f
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-08-27 16:35:38 +0900

    src/constants.ts を追加

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const NICOMEDKEY_ORIGIN = "https://nicomedkey.cc";
+export const BOT_USERNAME = "konton_freedom";
+export const NORMAL_COMMAND = "@konton_freedom 混沌ガチャ";
+export const PREMIUM_COMMAND = "@konton_freedom プレミアムポイントガチャ10連";
+export const PREMIUM_THRESHOLD = 3600;
+export const SLOT_DELAY_MS = 60_000;
+export const NORMAL_REPLY_TIMEOUT_MS = 6 * 60 * 60 * 1000;
+export const STALE_RESERVED_MS = 5 * 60 * 1000;
